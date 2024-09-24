23c54600f814137d208101b2f66fbafafca2c3a9
Sandilya Jandhyala <jysandilya@gmail.com>
2024-09-24 17:21:45 +0100

    Unreal top down template

---
+++ Source/SleepySumo/SleepySumo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SleepySumo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SleepySumo, "SleepySumo" );
+
+DEFINE_LOG_CATEGORY(LogSleepySumo)
+ 
---
+++ Source/SleepySumo/SleepySumoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SleepySumoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASleepySumoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASleepySumoGameMode();
+};
+
+
+
